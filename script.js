8164029346ab3543eefd905f2011c0a573fd495f
Alexander Leino <Alexander.Leino@yahoo.com>
2022-07-13 15:49:33 -0400

    push for staging

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let goToConnectAuthToken;
 let portalUserAuthToken;
 let callEventArray = [];
 const nextURL = "/";
-let serverURL = "http://localhost:3001";
+let serverURL = "https://braustin-server-staging.herokuapp.com";
 let oauth;
 
 //All that is occuring in the context will need to be handled in the script
